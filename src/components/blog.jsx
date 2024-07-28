f49d290e3a9d60745d70338cd7a440f49425f3af
gurubrahmam <vgurubrahmam338@gmail.com>
2024-07-28 21:58:26 +0530

    changes

--- src/components/blog.jsx
+++ src/components/blog.jsx
@@ -10,6 +10,7 @@ function Blog() {
   const navigate = useNavigate();
   const [blogs, setBlogs] = useState([]);
   const [selectedPost, setselectedPost] = useState(null);
+  const [searchItem,setSearchItem]=useState("")
   // const [isUpdateClicked, setIsUpdateClicked] = useState(false)
   const [updatepost, setUpdatepost] = useState({
     _id: "",
@@ -138,7 +139,15 @@ function Blog() {
         //   imageurl: "",
         // });
       });
+
+      //serch item
+      
+      
   };
+  const handleSearchChange=(e)=>{
+    setSearchItem(e.target.value)
+}
+const filteredBlogs=blogs.filter(blog=>blog.category.toLowerCase().includes(searchItem.toLowerCase()))
   return (
     <div>
       {!selectedPost && (
@@ -294,7 +303,7 @@ function Blog() {
                 </div>
               </div>
               <div className='text-start mt-5 d-md-none ml-5'>
-                    <input className='text-left searchpost text-secondary w-50' type='search' placeholder='Search Post Category..' />
+                    <input className='text-left searchpost text-secondary w-50' type='search' placeholder='Search Post Category..' value={searchItem} onChange={handleSearchChange} />
                     
                 </div>
 
@@ -302,114 +311,119 @@ function Blog() {
           
               <div className="col-md-9 cards-data align-items-center">
                 <div className="row">
-                  {blogs.map((blog, index) => (
-                    <div className="col-lg-4 col-md-6 mb-4" key={index}>
-                      <div className="card h-auto">
-                        <img
-                          src={blog.imageurl}
-                          className="card-img-top"
-                          alt={blog.title}
-                        />
-                        <div className="card-body d-flex flex-column">
-                          <div className="d-flex flex-row justify-content-between">
-                            <div>
-                              <img
-                                src="https://img.freepik.com/premium-photo/male-female-profile-avatar-user-avatars-gender-icons_1020867-75336.jpg"
-                                className="profileimg mb-2"
-                              ></img>
-                            </div>
+                  {filteredBlogs.length>0?(
+filteredBlogs.map((blog) => (
+  <div className="col-lg-4 col-md-6 mb-4" key={blog.id}>
+    <div className="card h-auto">
+      <img
+        src={blog.imageurl}
+        className="card-img-top"
+        alt={blog.title}
+      />
+      <div className="card-body d-flex flex-column">
+        <div className="d-flex flex-row justify-content-between">
+          <div>
+            <img
+              src="https://img.freepik.com/premium-photo/male-female-profile-avatar-user-avatars-gender-icons_1020867-75336.jpg"
+              className="profileimg mb-2"
+            ></img>
+          </div>
 
-                            <div className="">
-                              <p
-                                className="username"
-                                onChange={handlesubmitchange}
-                              >
-                                {blog.userId.name}
-                              </p>
-                              <p className="card-text fw-bold">
-                                {formatDate(blog.createdAt)}
-                              </p>
-                              <p className="card-text fw-bold">
-                                {formatTime(blog.createdAt)}
-                              </p>
-                            </div>
-                          </div>
-                          <h5
-                            className="card-title fw-bold"
-                            onChange={handlesubmitchange}
-                          >
-                            {blog.title}
-                          </h5>
-                          {/* <p className="card-text">{blog.userId.name}</p> */}
-                          <p
-                            className="card-text m-2 fs-5"
-                            onChange={handlesubmitchange}
-                          >
-                            {blog.category}
-                          </p>
+          <div className="">
+            <p
+              className="username"
+              onChange={handlesubmitchange}
+            >
+              {blog.userId.name}
+            </p>
+            <p className="card-text fw-bold">
+              {formatDate(blog.createdAt)}
+            </p>
+            <p className="card-text fw-bold">
+              {formatTime(blog.createdAt)}
+            </p>
+          </div>
+        </div>
+        <h5
+          className="card-title fw-bold"
+          onChange={handlesubmitchange}
+        >
+          {blog.title}
+        </h5>
+        {/* <p className="card-text">{blog.userId.name}</p> */}
+        <p
+          className="card-text m-2 fs-5"
+          onChange={handlesubmitchange}
+        >
+          {blog.category}
+        </p>
 
-                          <p
-                            className="card-text limited-height fs-6"
-                            onChange={handlesubmitchange}
-                          >
-                            {blog.description}
-                          </p>
-                          <div className=" mt-4 d-flex flex-spacebetween justify-content-between">
-                            <div className="">
-                              <Link
-                                to="#"
-                                className="btn btn-secondary fw-bold"
-                                onClick={() => handleDetailviewbtn(index)}
-                              >
-                                Detail View
-                              </Link>
-                            </div>
-                            <div className="d-flex flex-row justify-content-end">
-                              <button
-                                className="btn btn-Dark" data-bs-toggle="modal" data-bs-target="#staticBackdrop"
-                                onClick={() => {
-                                  
-                                  setUpdatepost(blog)
-                                }}
-                              >
-                                
-                                  <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="16"
-                                    height="16"
-                                    fill="currentColor"
-                                    className="bi bi-pencil-square"
-                                    viewBox="0 0 16 16"
-                                  >
-                                    <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
-                                    <path
-                                      fillRule="evenodd"
-                                      d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"
-                                    />
-                                  </svg>
-                                
-                              </button>
-                              <button
-                                className="btn btn-Dark"
-                                onClick={() => handleDelete(blog)}
-                              >
-                                <svg
-                                  xmlns="http://www.w3.org/2000/svg"
-                                  width="16"
-                                  height="16"
-                                  fill="currentColor"
-                                  className="bi bi-trash3"
-                                  viewBox="0 0 16 16"
-                                >
-                                  <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5" />
-                                </svg>
-                              </button>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  ))}
+        <p
+          className="card-text limited-height fs-6"
+          onChange={handlesubmitchange}
+        >
+          {blog.description}
+        </p>
+        <div className=" mt-4 d-flex flex-spacebetween justify-content-between">
+          <div className="">
+            <Link
+              to="#"
+              className="btn btn-secondary fw-bold"
+              onClick={() => handleDetailviewbtn(index)}
+            >
+              Detail View
+            </Link>
+          </div>
+          <div className="d-flex flex-row justify-content-end">
+            <button
+              className="btn btn-Dark" data-bs-toggle="modal" data-bs-target="#staticBackdrop"
+              onClick={() => {
+                
+                setUpdatepost(blog)
+              }}
+            >
+              
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-pencil-square"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                  <path
+                    fillRule="evenodd"
+                    d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"
+                  />
+                </svg>
+              
+            </button>
+            <button
+              className="btn btn-Dark"
+              onClick={() => handleDelete(blog)}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-trash3"
+                viewBox="0 0 16 16"
+              >
+                <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5" />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+))):(<div className="col-12 text-center">
+  <h5>Post Not Found</h5>
+</div>
+                  )}
+                  
                 </div>
               </div>
             </div>
